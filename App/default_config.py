b60777dc4f40ec66810614a03bb1adb1425b3573
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2025-04-13 13:55:21 -0400

    Update default_config.py

--- App/default_config.py
+++ App/default_config.py
@@ -4,7 +4,7 @@ SECRET_KEY = "secret key"
 # --- Mail Configuration ---
 MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 587
-MAIL_USERNAME = 'rajiv.lakhan25@gmail.com'
-MAIL_PASSWORD = 'ufos jeke orci ymjy'
+MAIL_USERNAME = 'preopassessmentteam@gmail.com'
+MAIL_PASSWORD = 'rxzd fvjr nlmy xrbv'
 MAIL_USE_TLS = True
 MAIL_USE_SSL = False
