b94c10a6fe1346603f9b81ce4c6ebe2dada269a5
Rajiv-INFO3604 <130197749+Rajiv-INFO3604@users.noreply.github.com>
2025-04-14 12:59:39 -0400

    Update questionnaire.py

--- App/models/questionnaire.py
+++ App/models/questionnaire.py
@@ -1,6 +1,8 @@
 from App.database import db
 from datetime import datetime
 import uuid
+from sqlalchemy.dialects.postgresql import JSON
+from sqlalchemy.ext.mutable import MutableDict
 
 def generate_short_uuid():
     return str(uuid.uuid4())[:8]
@@ -9,9 +11,12 @@ class Questionnaire(db.Model):
     __tablename__ = 'questionnaire'
     id = db.Column(db.String(20), primary_key=True, default=generate_short_uuid, server_default='gen_random_uuid()')
     patient_id = db.Column(db.String(20), db.ForeignKey('patient.id'))
-    responses = db.Column(db.JSON, nullable=True)  
-    previous_responses = db.Column(db.JSON, nullable=True)  
+    responses = db.Column(MutableDict.as_mutable(JSON), default={})
+    previous_responses = db.Column(MutableDict.as_mutable(JSON), default={})  
     flagged_questions = db.Column(db.JSON, nullable=True)  
+    doctor_flagged_questions = db.Column(db.JSON, nullable=True)
+    anesthesiologist_updates = db.Column(db.JSON, default=dict)
+    doctor_updates = db.Column(db.JSON, default=dict)
     operation_date = db.Column(db.String(20), nullable=True)
     status = db.Column(db.String(120), nullable=False, default='pending')
     evaluation_notes = db.Column(db.String(1200), nullable=True)
